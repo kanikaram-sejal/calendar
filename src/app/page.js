57af66197be0c5b7a3baeec85f01b1fc95ed2062
kanikaram-sejal <kanikaramsejal@gmail.com>
2024-11-21 16:29:35 -0800

    Added most of the  Next.js and CSS elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,197 @@
-import Image from "next/image";
+'use client'; 
 import styles from "./page.module.css";
+import {format, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, isSameMonth, isSameDay} from 'date-fns';
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [days, setDays] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [editingEvent, setEditingEvent] = useState(null);
+  const [selectedEventID, setSelectedEventID] = useState(null);
+
+  useEffect(()=> {
+    const startDate = startOfWeek(startOfMonth(currentDate));
+    const endDate = endOfWeek(endOfMonth(currentDate));
+
+    const calendarDays = [];
+    let day = startDate;
+
+    while(day <= endDate){
+      calendarDays.push(day);
+      day = addDays(day, 1);
+    }
+    setDays(calendarDays);
+  }, [currentDate]);
+
+  const handlePrevMonth = () => {
+    setCurrentDate(prev => addDays(startOfMonth(prev), -1));
+  };
+
+  const handleNextMonth = () => {
+    setCurrentDate(prev => addDays(endOfMonth(prev), 1));
+  };
+
+  const handleToday = () => {
+    setCurrentDate(new Date());
+  };
+
+  const openModal = (date, event = null) => {
+    setSelectedDate(date || new Date());
+    if(event){
+      setEditingEvent(event);
+      setEventTitle(event.title);
+      setEventDescription(event.description);
+    } else {
+      setEditingEvent(null);
+      setEventTitle("");
+      setEventDescription("");
+    }
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleEventSubmit = () => {
+    if(eventTitle.trim !== ""){
+      const newEvent = {
+        id: Date.now(),
+        title: eventTitle, 
+        description: eventDescription,
+        date: selectedDate,
+      }
+
+      if(editingEvent){
+        setEvents(events.map(event => event.id === editingEvent.id ? newEvent : event));
+      } else {
+        setEvents([...events, newEvent]);
+      }
+
+      setEventTitle("");
+      setEventDescription("");
+      setSelectedDate(new Date());
+      closeModal();
+    }
+  };
+
+  const handleEventChange = (e) => {
+    const {name, value} = e.target;
+    if(name === "title"){
+      setEventTitle(value);
+    } else if(name === "description"){
+      setEventDescription(value);
+    }
+  };
+
+  const handleDeleteEvent = (eventID) => {
+    setEvents(events.filter(event => event.id != eventID));
+    setSelectedEventID(null);
+  };
+
+  const handleEventClick = (eventId) => {
+    setSelectedEventID(prevId => (prevId === eventId ? null : eventId)); 
+  };
+  
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <h1>Calendar</h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.calendar}>
+        <div className={styles.header}>
+          <button className={styles.today} onClick={handleToday}>Today</button>
+          <div className={styles.center}>
+            <h2>{format(currentDate, "MMMM yyyy")}</h2>
+          </div>
+          <div className={styles.nav}>
+            <button onClick={handlePrevMonth}>{"<"}</button>
+            <button onClick={handleNextMonth}>{">"}</button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className={styles.days}>
+          {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, idx) => (
+              <div key={idx} className={styles.day}>{day}</div>))}
+        </div>
+
+        <div className={styles.grid}>
+          {days.map((date, idx) => (
+            <div
+              key={idx}
+              className={[styles.calendarDay, !isSameMonth(date, currentDate) && styles.inactive].filter(Boolean).join(' ')}
+              onClick={() => openModal(date)}>
+                <div className={styles.date}>{format(date, "d")}</div>
+                {events
+                  .filter(event => isSameDay(event.date, date))
+                  .map(event => (
+                    <div
+                      key={event.id}
+                      className={styles.event}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleEventClick(event.id); }}>
+                      <div>{event.title}</div>
+                      {selectedEventID === event.id && (
+                        <div className={styles.actions}>
+                          <button
+                          className={styles.edit}
+                          onClick={(e) => {
+                            e.stopPropagation(); 
+                            openModal(date, event); 
+                          }}>Edit</button>
+                          <button 
+                          className={styles.delete}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleDeleteEvent(event.id);
+                          }}>Delete</button>
+                        </div>
+                      )}
+                  </div>
+                ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <button className={styles.addevent} onClick={() => openModal(new Date())}>Add Event</button>
+        {isModalOpen && (
+          <div className={styles.modal}>
+            <div className={styles.content}>
+              <h3>{editingEvent ? "Edit Event" : "Add Event"}</h3>
+              <div className={styles.inputInfo}>
+                <input
+                  type="text"
+                  name="title"
+                  value={eventTitle}
+                  onChange={handleEventChange}
+                  placeholder="Event Title"
+                />
+                <textarea
+                  name="description"
+                  value={eventDescription}
+                  onChange={handleEventChange}
+                  placeholder="Event Description"
+                />
+                <label>Event Date:
+                  <input
+                    type="date"
+                    value={format(selectedDate, "yyyy-MM-dd")}
+                    onChange={(e) => setSelectedDate(new Date(e.target.value))}
+                  /> </label>
+              </div>
+              <button onClick={handleEventSubmit}>
+                {editingEvent ? "Save Changes" : "Save Event"}
+              </button>
+              <button onClick={closeModal}>Close</button>
+            </div>
+          </div>
+        )}
     </div>
   );
 }
